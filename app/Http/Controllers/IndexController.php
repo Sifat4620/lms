fb93cf54a2bd724caa4c2fd04e3627659e30ee21
Fahim Faisal Sifat <81096504+Sifat4620@users.noreply.github.com>
2025-03-08 21:42:16 +0600

    All are fine here is i change index controller and index blade file

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,14 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Models\BorrowedBook;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
    public function index() 
    {
+       // Fetch the total number of books
+       $totalBooks = Book::count();
 
-       return view('Dashboard.main.index');  
+       // Fetch the number of borrowed books
+       $borrowedBooks = BorrowedBook::count();
 
+       // Fetch the number of books that are not borrowed (in stock)
+       $inStockBooks = Book::whereNotIn('id', BorrowedBook::pluck('book_id'))->count();
+
+       // Calculate progress percentages
+       $borrowedPercentage = ($totalBooks > 0) ? ($borrowedBooks / $totalBooks) * 100 : 0;
+       $inStockPercentage = ($totalBooks > 0) ? ($inStockBooks / $totalBooks) * 100 : 0;
+
+       // Return view with dynamic data
+       return view('Dashboard.main.index', compact('totalBooks', 'borrowedBooks', 'inStockBooks', 'borrowedPercentage', 'inStockPercentage'));
    }
 }
--- resources/views/Dashboard/main/index.blade.php
+++ resources/views/Dashboard/main/index.blade.php
@@ -1,4 +1,5 @@
 @extends('Dashboard.main.master')
+
 @section('content')
 <div class="content-body">
     <div class="container">
@@ -14,14 +15,26 @@
             </div>
         </div>
 
+        <!-- Clock Display Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h4>Current Time: <span id="clock" class="text-primary"></span></h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Dashboard Metrics Section -->
         <div class="row">
             <div class="col-lg-4">
                 <div class="card">
                     <div class="card-body">
                         <h4>Total Books <span class="pull-right"><i class="ion-android-download f-s-30 text-primary"></i></span></h4>
-                        <h6 class="m-t-20 f-s-14">50% Complete</h6>
+                        <h6 class="m-t-20 f-s-14">{{ $totalBooks }} Books</h6>
                         <div class="progress m-t-0 h-7px">
-                            <div role="progressbar" class="progress-bar bg-primary wow animated progress-animated w-50pc h-7px"></div>
+                            <div role="progressbar" class="progress-bar bg-primary wow animated progress-animated w-{{ $borrowedPercentage }}pc h-7px"></div>
                         </div>
                     </div>
                 </div>
@@ -30,9 +43,9 @@
                 <div class="card">
                     <div class="card-body">
                         <h4>Total Borrow Books <span class="pull-right"><i class="ion-android-upload f-s-30 text-success"></i></span></h4>
-                        <h6 class="m-t-20 f-s-14">90% Complete</h6>
+                        <h6 class="m-t-20 f-s-14">{{ $borrowedBooks }} Borrowed</h6>
                         <div class="progress m-t-0 h-7px">
-                            <div role="progressbar" class="progress-bar bg-danger wow animated progress-animated w-90pc h-7px"></div>
+                            <div role="progressbar" class="progress-bar bg-danger wow animated progress-animated w-{{ $borrowedPercentage }}pc h-7px"></div>
                         </div>
                     </div>
                 </div>
@@ -41,10 +54,10 @@
             <div class="col-lg-4">
                 <div class="card">
                     <div class="card-body">
-                        <h4>In Stocks Books <span class="pull-right"><i class="ion-android-list f-s-30 text-danger"></i></span></h4>
-                        <h6 class="m-t-20 f-s-14">65% Checked</h6>
+                        <h4>In Stock Books <span class="pull-right"><i class="ion-android-list f-s-30 text-danger"></i></span></h4>
+                        <h6 class="m-t-20 f-s-14">{{ $inStockBooks }} In Stock</h6>
                         <div class="progress m-t-0 h-7px">
-                            <div role="progressbar" class="progress-bar bg-success wow animated progress-animated w-65pc h-7px"></div>
+                            <div role="progressbar" class="progress-bar bg-success wow animated progress-animated w-{{ $inStockPercentage }}pc h-7px"></div>
                         </div>
                     </div>
                 </div>
@@ -52,4 +65,23 @@
         </div>
     </div>
 </div>
+
+<script>
+    // Function to update the clock every second
+    function updateClock() {
+        const clockElement = document.getElementById("clock");
+        const currentTime = new Date();
+        const hours = currentTime.getHours().toString().padStart(2, '0');
+        const minutes = currentTime.getMinutes().toString().padStart(2, '0');
+        const seconds = currentTime.getSeconds().toString().padStart(2, '0');
+        const timeString = `${hours}:${minutes}:${seconds}`;
+        
+        clockElement.textContent = timeString;
+    }
+
+    // Update the clock immediately and then every second
+    updateClock();
+    setInterval(updateClock, 1000);
+</script>
+
 @endsection
